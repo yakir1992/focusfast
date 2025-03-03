038268b829eab4148f399f339a575c129515961c
yakir1992 <yakir1992@gmail.com>
2025-03-03 15:20:15 +0200

    UI Changes

--- components/Providers.js
+++ components/Providers.js
@@ -1,14 +1,11 @@
 'use client';
 
 import { SessionProvider } from "next-auth/react";
-import { ThemeProvider } from '@/contexts/ThemeContext';
 
 export default function Providers({ children }) {
   return (
     <SessionProvider>
-      <ThemeProvider>
-        {children}
-      </ThemeProvider>
+
     </SessionProvider>
   );
 }
