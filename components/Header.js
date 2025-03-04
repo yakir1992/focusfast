135fbfe408c3bb94c4010455f22bef59fac188e4
yakir1992 <yakir1992@gmail.com>
2025-03-04 21:38:10 +0200

    header changes

--- components/Header.js
+++ components/Header.js
@@ -13,10 +13,10 @@ const links = [
     href: "/#pricing",
     label: "Pricing",
   },
-  {
-    href: "/#testimonials",
-    label: "Reviews",
-  },
+  // {
+  //   href: "/#testimonials",
+  //   label: "Reviews",
+  // },
   {
     href: "/#faq",
     label: "FAQ",
