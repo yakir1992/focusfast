d5493c07a5e0ac67d8bdd8b5f18c5159f0a2a490
yakir1992 <yakir1992@gmail.com>
2025-03-05 11:54:10 +0200

    changes mongodb

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/libs/next-auth";
 
+// This is the correct way to export handlers in App Router
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST };
--- libs/next-auth.js
+++ libs/next-auth.js
@@ -1,55 +1,24 @@
 import GoogleProvider from "next-auth/providers/google";
-import EmailProvider from "next-auth/providers/email";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import config from "@/config";
 import connectMongo from "./mongoose";
 import NextAuth from "next-auth";
 
-
 export const authOptions = {
   debug: true,
-  // Set any random key in .env.local
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
-      // Follow the "Login with Google" tutorial to get your credentials
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      authorizationUrl: "https://accounts.google.com/o/oauth2/auth?prompt=consent&access_type=offline&response_type=code",
-      async profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.given_name ? profile.given_name : profile.name,
-          email: profile.email,
-          image: profile.picture,
-          createdAt: new Date(),
-        };
-      },
     }),
-    // Follow the "Login with Email" tutorial to set up your email server
-    // Requires a MongoDB database. Set MONOGODB_URI env variable.
-    ...(connectMongo
-      ? [
-        EmailProvider({
-          server: process.env.EMAIL_SERVER,
-          from: config.mailgun.fromNoReply,
-        }),
-      ]
-      : []),
   ],
-  // New users will be saved in Database (MongoDB Atlas). Each user (model) has some fields like name, email, image, etc..
-  // Requires a MongoDB database. Set MONOGODB_URI env variable.
-  // Learn more about the model type: https://next-auth.js.org/v3/adapters/models
-  ...(connectMongo && {
-    adapter: MongoDBAdapter(connectMongo, {
-      databaseName: "focusfast",
-    }),
+  adapter: MongoDBAdapter(connectMongo(), {
+    databaseName: "focusfast",
   }),
-
   callbacks: {
-    session: async ({ session, token }) => {
+    session: async ({ session, user }) => {
       if (session?.user) {
-        session.user.id = token.sub;
+        session.user.id = user.id;
       }
       return session;
     },
@@ -57,12 +26,6 @@ export const authOptions = {
   session: {
     strategy: "database",
     maxAge: 30 * 24 * 60 * 60,
-  },
-  theme: {
-    brandColor: config.colors.main,
-    // Add you own logo below. Recommended size is rectangle (i.e. 200x50px) and show your logo + name.
-    // It will be used in the login flow to display your logo. If you don't add it, it will look faded.
-    logo: `https://${config.domainName}/logoAndName.png`,
   },
 };
 
