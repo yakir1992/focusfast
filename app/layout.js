cbda8b91296ed1649c39d9191c553e1bb6c26d52
yakir1992 <yakir1992@gmail.com>
2025-03-07 02:35:33 +0200

    favicon added

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,6 @@ export default function RootLayout({ children }) {
         {config.domainName && (
           <PlausibleProvider domain={config.domainName} />
         )}
-        <link rel="icon" href="/favicon.ico" />
       </head>
       <body className="bg-neutral-900 text-white">
         <ClientLayout>{children}</ClientLayout>
